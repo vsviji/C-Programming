48928f75c5919b623af69bff2b47d620c8b1ac8c
Vijayalakshmi <127862956+vsviji@users.noreply.github.com>
2023-10-19 22:26:57 +0530

    Day-20

---
+++ 21.reversestring.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char original[] = "Hello, World!";
+    char reversed[100]; // Assuming a maximum length of 100 characters for the reversed string
+    int length = strlen(original);
+    
+    for (int i = 0; i < length; i++) {
+        reversed[i] = original[length - i - 1];
+    }
+    reversed[length] = '\0'; // Null-terminate the reversed string
+
+    printf("%s\n", reversed);
+    return 0;
+}
