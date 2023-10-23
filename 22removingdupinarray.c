e295a893478a0b1eacb301b56e27c6f43e9856db
Vijayalakshmi <127862956+vsviji@users.noreply.github.com>
2023-10-23 23:42:27 +0530

    Day-18

---
+++ 22removingdupinarray.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {1, 2, 2, 3, 4, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; ) {
+            if (arr[i] == arr[j]) {
+                for (int k = j; k < n - 1; k++) {
+                    arr[k] = arr[k + 1];
+                }
+                n--;
+            } else {
+                j++;
+            }
+        }
+    }
+    
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    
+    return 0;
+}
